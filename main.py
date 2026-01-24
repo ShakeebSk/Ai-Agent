638e97730c66da3e6a373eb393e2d8e039d57348
Shakeeb Shaikh <eren.85yeager@gmail.com>
2026-01-24 22:35:33 +0530

    Refactor main function and add CLI class
    Refactor main function to use CLI class and Click for command-line arguments.

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
+from typing import Any
 from client.llm_client import LLMClient
 import asyncio
+import click
 
 r""" 
+
 """
 
 
-async def main():
+class CLI:
+    def __init__(self):
+        pass
+
+    def run_single(self):
+        pass
+
+
+async def run(messages: dict[str, Any]):
     client = LLMClient()
-    messages = [{"role": "user", "content": "whats's up"}]
     async for event in client.chat_completion(messages, True):
         print(event)
+
+
+@click.command()
+@click.argument("prompt", required=False)
+def main(prompt: str | None):
+    print(prompt)
+    client = LLMClient()
+    messages = [{"role": "user", "content": prompt}]
+    # async for event in client.chat_completion(messages, True):
+    #     print(event)
+    asyncio.run(run(messages))
     print("Done")
 
 
-asyncio.run(main())
+main()
