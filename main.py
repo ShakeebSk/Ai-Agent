933ce50429e6f2e2354430943e846003e393d891
Shakeeb Shaikh <eren.85yeager@gmail.com>
2026-02-02 12:35:17 +0530

    Integrate Agent into CLI for async message handling
    Refactor CLI class to use Agent for processing messages asynchronously.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from typing import Any
+from agent.agent import Agent
+from agent.events import AgentEventType
 from client.llm_client import LLMClient
 import asyncio
 import click
@@ -10,28 +12,45 @@ r"""
 
 class CLI:
     def __init__(self):
-        pass
+        self.agent = Agent() | None = None
+        
 
-    def run_single(self):
-        pass
+    async def run_single(self,message: str):
+        async with Agent() as agent:
+            self.agent = agent
+            self._process_message(message)
 
+    async def _process_message(self, message: str) -> str | None:
+        if self.agent:
+            return None
+        
+        async for event in self.agent.run(message):
+            if event.type == AgentEventType.TEXT_DELTA:
+                content = event.data.get("content", "")
+                print(content, end="", flush=True)
+                
 
-async def run(messages: dict[str, Any]):
-    client = LLMClient()
-    async for event in client.chat_completion(messages, True):
-        print(event)
+# async def run(messages: dict[str, Any]):
+    # client = LLMClient()
+    # async for event in client.chat_completion(messages, True):
+    #     print(event)
+    # pass
 
 
 @click.command()
 @click.argument("prompt", required=False)
-def main(prompt: str | None):
-    print(prompt)
-    client = LLMClient()
-    messages = [{"role": "user", "content": prompt}]
+def main(prompt: str | None,):
+    # print(prompt)
+    # client = LLMClient()
+    cli = CLI()
+    
+    # messages = [{"role": "user", "content": prompt}]
     # async for event in client.chat_completion(messages, True):
     #     print(event)
-    asyncio.run(run(messages))
-    print("Done")
+    if prompt:
+        asyncio.run(cli.run_single(prompt))
+    # asyncio.run(run(messages))
+    # print("Done")
 
 
 main()
